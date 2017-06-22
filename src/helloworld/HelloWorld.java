4168313289df63d7079986cfec5dd2b74978f407
Kirillov vasilii <vkirillov@bmail.ru>
2017-06-22 09:13:24 +0400

    helloworld

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld;
+
+/**
+ *
+ * @author Vasilii_Kirillov
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("What's the answer on The Ultimate Question of Life, the Universe, and Everything..?");
+        System.out.println("\t42");
+    }
+    
+}
